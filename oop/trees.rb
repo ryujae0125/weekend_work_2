04c807f4969674b46a056e7cb3c6f7c721bedac5
Jae Ryu <ryu.jae0125@gmail.com>
2016-07-25 19:42:40 -0400

    added answers to all except one

--- oop/trees.rb
+++ oop/trees.rb
@@ -3,8 +3,25 @@
 # It should have a bear_fruit? method which should return true if the tree has fruit that year. the tree produces fruit when it has more than 7 rings but less than 15, but false otherwise.
 # The class should also have an winter_season method that increases the rings attribute by 1.
 # Hint: remember that with object oriented programming the object holds values in the attributes to describe it's current state. Even when we aren't reading it's attributes, they should be up to date.
+class Tree
+  attr_reader :rings
+  def initialize
+    @rings = 0
+    @fruits = false
+  end
 
+  def bear_fruit?
+    if @rings > 7 and @rings < 15
+      @fruits = true
+    else
+      @Fruits = false
+    end
+  end
 
+  def winter_season
+    @rings += 1
+  end
+end
 # Driver code - don't touch anything below this line.
 puts "TESTING the Tree class..."
 puts
